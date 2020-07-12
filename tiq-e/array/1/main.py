04953d4e433b704fc77f2e1bf4dd3a2358007d8f
Max <max@aiwon.ru>
2020-07-12 19:26:38 +0300

    TIQ-E::Array::1 : Remove Duplicates from Sorted Array.

--- tiq-e/array/1/main.py
+++ tiq-e/array/1/main.py
@@ -7,11 +7,6 @@ class Solution(object):
         l = len(nums)
         s = 0
         for i in range(l):
-            # i = 0 ; s = 0 /1
-            # i = 1 ; s = 0 > s = 1 > nums s|1 = num i|1    /2
-            # i = 2; s = 1 > s = 2 > nums s|2 = num i|2 /3
-            # i = 3; s = 2 > s = 2 > nums s|3 = num i|3 /3
-            # i = 4 /3
             if nums[i] != nums[s]:
                 s += 1
                 nums[s] = nums[i]
