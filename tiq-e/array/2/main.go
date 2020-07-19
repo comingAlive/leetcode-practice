3ff5906a5e0cdb4506b483bed8b7a53ee183c130
Max <max@aiwon.ru>
2020-07-19 23:45:04 +0300

    TIQ-E_Array-2 : Best Time to Buy and Sell Stock II.

---
+++ tiq-e/array/2/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func maxProfit(prices []int) int {
+	profit := 0
+	for i := 1; i < len(prices); i++ {
+		if prices[i] > prices[i-1] {
+			profit += prices[i] - prices[i-1]
+		}
+	}
+	return profit
+}
+
+func main() {
+	fmt.Println(maxProfit([]int{1, 1, 2, 3, 4}))
+}
